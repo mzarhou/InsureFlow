ac05d57ca7bcc8740fd25ee64eb6df1c4703530b
zarhou <zarhou.cc@gmail.com>
2021-06-20 15:50:04 +0100

    fix: add label to 1er mise en label - etap 2

--- resources/js/Components/sections/GestionClient/Create/Etap2.svelte
+++ resources/js/Components/sections/GestionClient/Create/Etap2.svelte
@@ -25,7 +25,8 @@ let options = [
     <input type="text" bind:value={$clientInfo.vehicule.marque_type} class="w-full px-4 py-2 mb-6 border rounded-md" placeholder="Marque et type" />
     <input type="text" bind:value={$clientInfo.vehicule.type_carrosserie} class="w-full px-4 py-2 mb-6 border rounded-md" placeholder="Type de carrosserie" />
     <input type="number" bind:value={$clientInfo.vehicule.poids_total_en_charge} class="w-full px-4 py-2 mb-6 border rounded-md" placeholder="Poids total en charge" />
-    <input type="date" bind:value={$clientInfo.vehicule.date_1er_mise_en} class="w-full px-4 py-2 mb-6 border rounded-md" placeholder="Date 1er mise en" />
+    <label for="mise-en" class="block mb-1 text-gray-800">Date 1er mise en: </label>
+    <input id="mise-en" type="date" bind:value={$clientInfo.vehicule.date_1er_mise_en} class="w-full px-4 py-2 mb-6 border rounded-md" placeholder="Date 1er mise en" />
     <input type="text" bind:value={$clientInfo.vehicule.usage} class="w-full px-4 py-2 mb-6 border rounded-md" placeholder="Usage: exp: C1X" />
     <input type="number" bind:value={$clientInfo.vehicule.nombre_places} class="w-full px-4 py-2 mb-6 border rounded-md" placeholder="Nombre de places" />
     <input type="number" bind:value={$clientInfo.vehicule.cylindree} class="w-full px-4 py-2 mb-6 border rounded-md" placeholder="Cylindree" />
